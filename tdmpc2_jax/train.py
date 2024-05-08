72b72b387d180ee76a65adc3dab342466bebed57
Julia Fu <wanjia_fu@brown.edu>
2024-05-07 23:55:39 -0400

    log info

--- tdmpc2_jax/train.py
+++ tdmpc2_jax/train.py
@@ -94,65 +94,79 @@ def train(cfg: dict):
   prev_plan = None
   minmax = MinMax()
   observation, _ = env.reset(seed=seed)
-  for i in tqdm.tqdm(range(max_steps), smoothing=0.1):
-    if i <= seed_steps:
-      action = env.action_sample()
-    else:
-      rng, action_key = jax.random.split(rng)
-      action, prev_plan = agent.act(
-          observation, minmax, prev_plan, train=True, key=action_key)
-    # print(f"Action: {action}")
-
-    next_observation, reward, terminated, truncated, info = env.step(action)
-    replay_buffer.insert(dict(
-        observation=observation,
-        action=action,
-        reward=reward,
-        next_observation=next_observation,
-        terminated=terminated,
-        truncated=truncated),
-        episode_index=ep_count)
-    observation = next_observation
-
-    if terminated or truncated:
-      observation, _ = env.reset()
-      prev_plan = None
-
-      r = info['episode']['r']
-      l = info['episode']['l']
-      print(f"Episode: r = {r}, l = {l}")
-      # Print the mean of all episode info
-      if len(ep_info) > 0:
-        print(jax.tree_map(lambda x: np.mean(x), ep_info))
-      ep_count += 1
-
-    if i >= seed_steps:
-      if i == seed_steps:
-        print('Pre-training on seed data...')
-        num_updates = seed_steps
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  log_file_path = os.path.join(script_dir, "log_result_reward.txt")
+  returns = []
+  losses = []
+  with open(log_file_path, "a") as log_file:
+    log_file.write("Step,Loss,Return,Return Std,Loss Std\n")
+    for i in tqdm.tqdm(range(max_steps), smoothing=0.1):
+      if i <= seed_steps:
+        action = env.action_sample()
       else:
-        num_updates = 1
+        rng, action_key = jax.random.split(rng)
+        action, prev_plan = agent.act(
+            observation, minmax, prev_plan, train=True, key=action_key)
+      # print(f"Action: {action}")
 
-      rng, *update_keys = jax.random.split(rng, num_updates+1)
-      for j in range(num_updates):
-        # print(f"Update {j}...")
-        batch = replay_buffer.sample(agent.batch_size, agent.horizon)
-        # batch['action] should be horizon x batch_size x action_dim, but it is batch_size x horizon
-        agent, train_info = agent.update(
-            observations=batch['observation'],
-            actions=batch['action'],
-            rewards=batch['reward'],
-            next_observations=batch['next_observation'],
-            terminated=batch['terminated'],
-            truncated=batch['truncated'],
-            key=update_keys[j])
-        # Append all episode info
-        if len(ep_info) == 0:
-          ep_info = train_info
+      next_observation, reward, terminated, truncated, info = env.step(action)
+      replay_buffer.insert(dict(
+          observation=observation,
+          action=action,
+          reward=reward,
+          next_observation=next_observation,
+          terminated=terminated,
+          truncated=truncated),
+          episode_index=ep_count)
+      observation = next_observation
 
-        if i % 100 == 0:
-          ep_info = jax.tree_map(
-              lambda x, y: np.append(np.array(x), np.array(y)), ep_info, train_info)
+      if terminated or truncated:
+        observation, _ = env.reset()
+        prev_plan = None
+
+        r = info['episode']['r']
+        l = info['episode']['l']
+        returns.append(r[0])
+        return_std = np.std(returns)
+        print(f"Episode: r = {r}, l = {l}")
+        # Print the mean of all episode info
+        if len(ep_info) > 0:
+          print(jax.tree_map(lambda x: np.mean(x), ep_info))
+          # log episode info to file
+          avg_loss = np.mean(ep_info['total_loss'])
+          losses = ep_info['total_loss']
+          loss_std = np.std(losses) 
+          log_file.write(f"{i}, {avg_loss}, {r[0]}, {return_std}, {loss_std}\n") 
+          log_file.flush() 
+        ep_count += 1
+
+      if i >= seed_steps:
+        if i == seed_steps:
+          print('Pre-training on seed data...')
+          num_updates = seed_steps
+        else:
+          num_updates = 1
+
+        rng, *update_keys = jax.random.split(rng, num_updates+1)
+        for j in range(num_updates):
+          # print(f"Update {j}...")
+          batch = replay_buffer.sample(agent.batch_size, agent.horizon)
+          # batch['action] should be horizon x batch_size x action_dim, but it is batch_size x horizon
+          agent, train_info = agent.update(
+              observations=batch['observation'],
+              actions=batch['action'],
+              rewards=batch['reward'],
+              next_observations=batch['next_observation'],
+              terminated=batch['terminated'],
+              truncated=batch['truncated'],
+              key=update_keys[j])
+          # Append all episode info
+          if len(ep_info) == 0:
+            ep_info = train_info
+
+          if i % 100 == 0:
+            ep_info = jax.tree_map(
+                lambda x, y: np.append(np.array(x), np.array(y)), ep_info, train_info)
 
 
 if __name__ == '__main__':
